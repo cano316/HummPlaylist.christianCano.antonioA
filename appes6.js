952cdd752d7950d916370dcade79adb34067b573
tonito98 <86890715+tonito98@users.noreply.github.com>
2022-05-31 00:25:34 +0200

    added the index and js files

---
+++ appes6.js
@@ -0,0 +1,148 @@
+class Song{
+    constructor(songTitle,artist){
+        this.songTitle = songTitle;
+        this.artist = artist;
+    }
+}
+
+class UI{
+    addSongToPlaylist(song){
+        const playlist = document.getElementById
+        ('play-list');
+        // Create tr element
+        const row = document.createElement('tr');
+        //Insert Columns
+        row.innerHTML = `
+        <td>${song.songTitle}</td>
+        <td>${song.artist}</td>
+        <td><a href="#" class="delete">X<a></td>
+        `;
+        playlist.appendChild(row);
+    }
+    showAlert(message, className){
+        //Create div
+        const div = document.createElement('div');
+        //Add classes
+        div.className = `alert ${className}`;
+        //Add text
+        div.appendChild(document.createTextNode (message));
+        // Get parent
+        const container = document.querySelector ('.container');
+        //Get form
+        const form = document.querySelector('#playlist-form')
+        //Insert alert
+        container.insertBefore(div, form);
+
+        //Timeout after 3 seconds
+        setTimeout(function(){
+            document.querySelector('.alert').remove();
+        }, 2000);
+    }
+    deleteSong(target){
+        if(target.className === 'delete'){ 
+            target.parentElement.parentElement.remove();
+        }
+    }
+    clearFields(){
+        document.getElementById('songTitle').value = '';
+        document.getElementById('artist').value = '';
+    }
+}
+
+// Local Storage Class
+    class Storage {
+        static getSongs(){
+            let songs;
+            if(localStorage.getItem('songs') === null) {
+                songs = [];
+            } else {
+                songs = JSON.parse(localStorage.getItem('songs'));
+            }
+            return songs;
+        }
+
+        static displaySong(){
+            const songs = Storage.getSongs();
+
+            songs.forEach(song => {
+                const ui = new UI;
+
+                // Add song to ui
+                ui.addSongToPlaylist(song);
+            });
+        }
+
+        static addSong(song){
+            const songs = Storage.getSongs();
+           
+            songs.push(song);
+
+            localStorage.setItem('songs', JSON.stringify(songs));
+        }
+
+        static removeSong(songTitle){
+            const songs = Storage.getSongs();
+
+            songs.forEach(function(song, index) {
+               if(song.songTitle === songTitle){
+                songs.splice(index, 1);
+               }
+            });
+
+            localStorage.setItem('songs', JSON.stringify(songs));
+        }
+    }
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Storage.displaySong);
+
+//Event Listener for add song
+document.getElementById('playlist-form')
+.addEventListener('submit', function(e){
+    // Get form values
+    const songTitle = document.getElementById('songTitle').value,
+        artist = document.getElementById
+        ('artist').value
+   //Instantiate book
+   const song = new Song(songTitle, artist);
+
+   //Instantiate UI
+   const ui = new UI();
+
+   // Validate
+   if(songTitle === '' || artist === ''){
+       //Error alert
+       ui.showAlert('Please fill out all fields','error');
+   } else {
+    // Add song to playlist
+    ui.addSongToPlaylist(song);
+
+    // Add to local storage
+    Storage.addSong(song);
+
+    //Show success
+    ui.showAlert('Song Added', 'success');
+
+    // Clear fields
+    ui.clearFields();   
+   }
+   e.preventDefault();
+});
+
+// Event Listener for delete
+document.getElementById('play-list')
+.addEventListener('click', function(e){
+
+    // Instantiate UI
+    const ui = new UI();
+    
+    // Delete song
+    ui.deleteSong(e.target);
+    // Remove from local storage
+    Storage.removeSong(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+
+    // Show message
+    ui.showAlert('Song Removed!', 'success');
+
+    e.preventDefault();
+});   
