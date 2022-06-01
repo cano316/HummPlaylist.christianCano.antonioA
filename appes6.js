552a20a6f5e58aa644d8afd8692578f8b9787a94
cano316 <christiancano316@gmail.com>
2022-05-31 19:28:20 -0700

    second commit

--- appes6.js
+++ appes6.js
@@ -1,148 +1,168 @@
-class Song{
-    constructor(songTitle,artist){
+class Song {
+    constructor(songTitle, artist) {
         this.songTitle = songTitle;
         this.artist = artist;
     }
 }
 
-class UI{
-    addSongToPlaylist(song){
+class UI {
+    addSongToPlaylist(song) {
         const playlist = document.getElementById
-        ('play-list');
+            ('play-list');
         // Create tr element
         const row = document.createElement('tr');
+
+        //Apply class to row
+
+        // row.classList.add('tableStyles')
         //Insert Columns
+        // row.innerHTML = `
+        // <td>${song.songTitle}</td>
+        // <td>${song.artist}</td>
+        // <td><a href="#" class="delete">X<a></td>
+        // `;
         row.innerHTML = `
         <td>${song.songTitle}</td>
         <td>${song.artist}</td>
-        <td><a href="#" class="delete">X<a></td>
+        <td><i class="fa-solid fa-trash-can delete"></i></td>
         `;
         playlist.appendChild(row);
     }
-    showAlert(message, className){
+    showAlert(message, className) {
         //Create div
         const div = document.createElement('div');
         //Add classes
         div.className = `alert ${className}`;
         //Add text
-        div.appendChild(document.createTextNode (message));
+        div.appendChild(document.createTextNode(message));
         // Get parent
-        const container = document.querySelector ('.container');
+        const container = document.querySelector('.container');
         //Get form
         const form = document.querySelector('#playlist-form')
         //Insert alert
         container.insertBefore(div, form);
 
         //Timeout after 3 seconds
-        setTimeout(function(){
+        setTimeout(function () {
             document.querySelector('.alert').remove();
         }, 2000);
     }
-    deleteSong(target){
-        if(target.className === 'delete'){ 
+    deleteSong(target) {
+        if (target.classList.contains('delete')){
             target.parentElement.parentElement.remove();
         }
     }
-    clearFields(){
+    clearFields() {
         document.getElementById('songTitle').value = '';
         document.getElementById('artist').value = '';
     }
 }
 
 // Local Storage Class
-    class Storage {
-        static getSongs(){
-            let songs;
-            if(localStorage.getItem('songs') === null) {
-                songs = [];
-            } else {
-                songs = JSON.parse(localStorage.getItem('songs'));
-            }
-            return songs;
+class Storage {
+    static getSongs() {
+        let songs;
+        if (localStorage.getItem('songs') === null) {
+            songs = [];
+        } else {
+            songs = JSON.parse(localStorage.getItem('songs'));
         }
-
-        static displaySong(){
-            const songs = Storage.getSongs();
-
-            songs.forEach(song => {
-                const ui = new UI;
-
-                // Add song to ui
-                ui.addSongToPlaylist(song);
-            });
-        }
-
-        static addSong(song){
-            const songs = Storage.getSongs();
-           
-            songs.push(song);
-
-            localStorage.setItem('songs', JSON.stringify(songs));
-        }
-
-        static removeSong(songTitle){
-            const songs = Storage.getSongs();
-
-            songs.forEach(function(song, index) {
-               if(song.songTitle === songTitle){
-                songs.splice(index, 1);
-               }
-            });
-
-            localStorage.setItem('songs', JSON.stringify(songs));
-        }
+        return songs;
     }
+
+    static displaySong() {
+        const songs = Storage.getSongs();
+
+        songs.forEach(song => {
+            const ui = new UI;
+
+            // Add song to ui
+            ui.addSongToPlaylist(song);
+        });
+    }
+
+    static addSong(song) {
+        const songs = Storage.getSongs();
+
+        songs.push(song);
+
+        localStorage.setItem('songs', JSON.stringify(songs));
+    }
+
+    static removeSong(songTitle) {
+        const songs = Storage.getSongs();
+
+        songs.forEach(function (song, index) {
+            if (song.songTitle === songTitle) {
+                songs.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('songs', JSON.stringify(songs));
+    }
+}
 
 // DOM Load Event
 document.addEventListener('DOMContentLoaded', Storage.displaySong);
 
 //Event Listener for add song
 document.getElementById('playlist-form')
-.addEventListener('submit', function(e){
-    // Get form values
-    const songTitle = document.getElementById('songTitle').value,
-        artist = document.getElementById
-        ('artist').value
-   //Instantiate book
-   const song = new Song(songTitle, artist);
+    .addEventListener('submit', function (e) {
+        // Get form values
+        const songTitle = document.getElementById('songTitle').value,
+            artist = document.getElementById
+                ('artist').value
+        //Instantiate book
+        const song = new Song(songTitle, artist);
 
-   //Instantiate UI
-   const ui = new UI();
+        //Instantiate UI
+        const ui = new UI();
 
-   // Validate
-   if(songTitle === '' || artist === ''){
-       //Error alert
-       ui.showAlert('Please fill out all fields','error');
-   } else {
-    // Add song to playlist
-    ui.addSongToPlaylist(song);
+        // Validate
+        if (songTitle === '' || artist === '') {
+            //Error alert
+            ui.showAlert('Please fill out all fields', 'error');
+        } else {
+            // Add song to playlist
+            ui.addSongToPlaylist(song);
 
-    // Add to local storage
-    Storage.addSong(song);
+            // Add to local storage
+            Storage.addSong(song);
 
-    //Show success
-    ui.showAlert('Song Added', 'success');
+            //Show success
+            ui.showAlert('Song Added', 'success');
 
-    // Clear fields
-    ui.clearFields();   
-   }
-   e.preventDefault();
-});
+            // Clear fields
+            ui.clearFields();
+        }
+        e.preventDefault();
+    });
 
 // Event Listener for delete
 document.getElementById('play-list')
-.addEventListener('click', function(e){
+    .addEventListener('click', function (e) {
 
-    // Instantiate UI
-    const ui = new UI();
-    
-    // Delete song
-    ui.deleteSong(e.target);
-    // Remove from local storage
-    Storage.removeSong(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+        // Instantiate UI
+        const ui = new UI();
 
-    // Show message
-    ui.showAlert('Song Removed!', 'success');
+        // Delete song
+        ui.deleteSong(e.target);
+        // Remove from local storage
+        Storage.removeSong(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
 
-    e.preventDefault();
-});   
+        // Show message
+        ui.showAlert('Song Removed!', 'success');
+
+        e.preventDefault();
+    });
+
+// Event Listener for clear button
+
+const clearButton = document.querySelector('#clearButton')
+
+clearButton.addEventListener('click', function () {
+
+    localStorage.clear();
+})
+
+//come back to this
